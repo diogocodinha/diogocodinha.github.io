5d997da99d65fe94326df8ddcea3219e0bedb4ed
Bruno Codinha <bruno.codinha@tecnico.ulisboa.pt>
2021-07-19 23:17:03 +0100

    new site

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,28 @@
+let currentPriceId;
+
 $(document).ready(function(){
+  let image;
+  $('#image').on("change", function() {
+
+      if (this.files && this.files[0]) {
+
+      var FR= new FileReader();
+
+      FR.addEventListener("load", function(e) {
+        image = e.target.result;
+      });
+
+      FR.readAsDataURL( this.files[0] );
+  }
+  });
+
+  $("#bidButton").on("click", function() {
+      $("#"+currentPriceId).text($('#modalPrice').val()+"€");
+  })
+
+  $("#sellButton").on('click', function() {
+    createItem(image, $('#name').val(), $('#description').val(), $('#price').val());
+  });
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
@@ -23,3 +47,33 @@ $(document).ready(function(){
     });
   });
 })
+
+let c = 7;
+
+function createItem(imagem, nome, descricao, preco) {
+  const priceId = "preco" + c;
+  const buttonId = "licitar" + c;
+  const li = `<li class="list-group-item clearfix">
+              <img class="img-responsive img-rounded" src="${imagem}" alt=""/>
+              <h3 class="list-group-item-heading">
+                ${nome}
+                <span class="label label-danger pull-right">NEW !</span>
+              </h3>
+              <p class="list-group-item-text lead">
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio.
+                <br />
+                <a href="#"><small>Details&#8230;</small></a>
+              </p>
+              <div class="btn-toolbar pull-right" role="toolbar" aria-label="">
+                <a class="btn btn-default" id="${buttonId}" data-toggle="modal" data-target="#bidModal">Licitar</a>
+                <a class="btn btn-primary" id="${priceId}">${preco}€</a>
+              </div>
+            </li>`;
+  document.getElementById("listaItens").innerHTML = li + document.getElementById("listaItens").innerHTML;
+
+  $("#"+buttonId).on("click", function() {
+      console.log(priceId, buttonId);
+      currentPriceId = priceId;
+  });
+  c++;
+}
